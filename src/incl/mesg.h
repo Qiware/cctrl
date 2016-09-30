42acb035cf664bc4c402fa2b3ab58bc4bdc39a2b
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-09-30 17:43:58 +0800

    [MESG]: 删除与业务相关的协议定义

--- src/incl/mesg.h
+++ src/incl/mesg.h
@@ -9,8 +9,6 @@
 #if !defined(__MESG_H__)
 #define __MESG_H__
 
-#include "uri.h"
-
 /* 通用协议头 */
 typedef struct
 {
@@ -67,74 +65,5 @@ typedef struct
     log_debug((log), "Call %s()! type:%d len:%d chksum:0x%X/0x%X sid:%lu nid:%u serial:%lu", \
             __func__, (head)->type, (head)->length, (head)->chksum, MSG_CHKSUM_VAL, \
             (head)->sid, (head)->nid, (head)->serial);
-
-////////////////////////////////////////////////////////////////////////////////
-/* 搜索消息结构 */
-#define SRCH_WORD_LEN       (128)
-typedef struct
-{
-    char words[SRCH_WORD_LEN];          /* 搜索关键字 */
-} mesg_search_req_t;
-
-////////////////////////////////////////////////////////////////////////////////
-/* 插入关键字-请求 */
-typedef struct
-{
-    char word[SRCH_WORD_LEN];           /* 关键字 */
-    char url[URL_MAX_LEN];              /* 关键字对应的URL */
-    int freq;                           /* 频率 */
-} mesg_insert_word_req_t;
-
-/* 插入关键字-应答 */
-typedef struct
-{
-#define MESG_INSERT_WORD_FAIL   (0)
-#define MESG_INSERT_WORD_SUCC   (1)
-    int code;                           /* 应答码 */
-    char word[SRCH_WORD_LEN];           /* 关键字 */
-} mesg_insert_word_rsp_t;
-
-#define mesg_insert_word_resp_hton(rsp) do { \
-    (rsp)->code = htonl((rsp)->code); \
-} while(0)
-
-#define mesg_insert_word_resp_ntoh(rsp) do { \
-    (rsp)->code = ntohl((rsp)->code); \
-} while(0)
-
-////////////////////////////////////////////////////////////////////////////////
-/* 订阅-请求 */
-typedef struct
-{
-    uint32_t type;                      /* 订阅类型(消息类型) */
-    int nid;                            /* 结点ID */
-} mesg_sub_req_t;
-
-#define mesg_sub_req_hton(req) do { /* 主机 -> 网络 */\
-    (req)->type = htonl((req)->type); \
-    (req)->nid = htonl((req)->nid); \
-} while(0)
-
-#define mesg_sub_req_ntoh(req) do { /* 网络 -> 主机 */\
-    (req)->type = ntohl((req)->type); \
-    (req)->nid = ntohl((req)->nid); \
-} while(0)
-
-/* 订阅-应答 */
-typedef struct
-{
-    int code;                           /* 应答码 */
-    uint32_t type;                      /* 订阅类型(消息类型) */
-} mesg_sub_rsp_t;
-
-#define mesg_sub_rsp_hton(rsp) do { /* 主机 -> 网络 */\
-    (rsp)->code = htonl((rsp)->code); \
-    (rsp)->type = htonl((rsp)->type); \
-} while(0)
-
-#define mesg_sub_rsp_ntoh(rsp) do { /* 网络 -> 主机 */\
-    (rsp)->code = ntohl((rsp)->code); \
-    (rsp)->type = ntohl((rsp)->type); \
-} while(0)
 
 #endif /*__MESG_H__*/
